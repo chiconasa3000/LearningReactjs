f00ec6442399306fbb5cf0cb6342cca0d6669aa2
chiconasa3000 <csucavelando@gmail.com>
2017-03-05 18:56:27 -0500

    Commit-InsercionVersionado: 	modificado:    README.md 	borrado:       package1.json 	modificado:    src/App.css 	modificado:    src/App.js 	nuevo archivo: src/imags/MyAvatar.jpg 	modificado:    src/index.css 	modificado:    src/index.js 	nuevo archivo: src1/App.css 	copied:        src/App.js -> src1/App.js 	renombrado:    src/App.test.js -> src1/App.test.js 	nuevo archivo: src1/index.css 	nuevo archivo: src1/index.js 	renombrado:    src/logo.svg -> src1/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import logo from './imags/MyAvatar.jpg';
 import './App.css';
 
+function getGreeting(user){
+	if(user){
+		return <h3>Hola Soy {formatName(user)}</h3>;
+	}
+	return <h1>Hola Soy un Extraño.</h1>;
+}
+
+function formatName(user){
+	return user.firstName + '_' + user.lastName + '@gmail.com'; 
+}
+
+const user = {
+	firstName: 'Christian',
+	lastName: 'Suca',
+	avatarUrl: logo
+};
+
+const element_B = (
+	<img src={user.avatarUrl}></img>
+);
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Bienvenido a React</h2>
-        </div>
-        <div className="App-container">
-          Hola Mundo Ancho y Ajeno y Feroz
-        </div>
-      </div>
-    );
-  }
+	render(){
+		return (
+			<div className="App">
+				<div className="App-header">
+				<img src={logo} className = "App-logo" alt="logo" />
+				<h2>Bienvenido a React</h2>
+				</div>
+				{getGreeting(user)}
+			</div>
+		);
+	}
 }
 
 export default App;
